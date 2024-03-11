ef42ab944e637843e9b82de03ce45fdf9abe6a66
Andrew Toth <andrewstoth@gmail.com>
2024-03-11 09:55:25 -0400

    Make options use camelCase

--- sdk/src/options.rs
+++ sdk/src/options.rs
@@ -4,18 +4,19 @@ use serde::Serialize;
 
 #[napi(object)]
 pub(crate) struct IntegrationOSOptions {
-  pub server_url: String,
+    pub server_url: String,
 }
 
 #[napi(object)]
 pub(crate) struct UnifiedOptions {
-  pub response_passthrough: Option<bool>,
-  pub passthrough_headers: Option<HashMap<String, String>>,
-  pub passthrough_query: Option<HashMap<String, String>>,
+    pub response_passthrough: Option<bool>,
+    pub passthrough_headers: Option<HashMap<String, String>>,
+    pub passthrough_query: Option<HashMap<String, String>>,
 }
 
 #[napi(object)]
 #[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 pub(crate) struct DeleteOptions {
-  pub modify_token: Option<String>,
+    pub modify_token: Option<String>,
 }
